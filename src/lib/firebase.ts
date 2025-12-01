7af0288220c601270209b54a57140c3e76f3094a
yeabsera sisay <g4uforlife@gmail.com>
2025-12-01 14:22:59 +0000

    npm install firebase // Import the functions you need from the SDKs you

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,16 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "test-api-key",
-  authDomain: "test-auth-domain",
-  projectId: "test-project-id",
-  storageBucket: "test-storage-bucket",
-  messagingSenderId: "test-messaging-sender-id",
-  appId: "test-app-id"
+  apiKey: "AIzaSyBtKas2Q9pV48iKFAwcPiktUTdY6mgskEE",
+  authDomain: "cozy-a2fbf.firebaseapp.com",
+  databaseURL: "https://cozy-a2fbf-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cozy-a2fbf",
+  storageBucket: "cozy-a2fbf.appspot.com",
+  messagingSenderId: "232046779269",
+  appId: "1:232046779269:web:cfe90b74e33bc8db6a42ec",
+  measurementId: "G-J9PXJZ5M9Z"
 };
 
 // Initialize Firebase
