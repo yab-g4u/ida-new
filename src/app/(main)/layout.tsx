843bdeb74f040198d3a797fd9d12877ba2c3c627
yeabsera sisay <g4uforlife@gmail.com>
2026-01-04 18:26:38 +0000

    remove the ai chatbot

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -14,7 +14,7 @@ export default function MainAppLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col">
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 pb-24">{children}</main>
       <PillNav items={navItems} />
     </div>
   );
