14070fdc2eaa19453a2f7a27254060b59e0fa22d
yeabsera sisay <g4uforlife@gmail.com>
2026-08-08 23:39:00 +0300

    Update page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 
 
-// Redirect users to the home page, where the auth flow will handle them.
+
 export default function SignupPage() {
   redirect('/home');
 }
